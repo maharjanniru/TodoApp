2102f0b13e5928169699163c1012a44f768fdf9a
maharjanniru <nmaharjan@deerhold.org>
2020-08-03 17:56:15 +0545

    init commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Todos from "./Todos";
+
+class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: "#f4f4f4",
+      padding: "10px",
+      borderBottom: "1px #cc dotted",
+      textDecoration: this.props.todo.completed ? "line-through" : "none",
+    };
+  };
+
+  // markComplete = (e) => {
+  //   console.log(this.props);
+  // };
+  render() {
+    //destructuring
+    const { id, title } = this.props.todo;
+    console.log(this.props.todo);
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />
+          {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>
+            x
+          </button>
+        </p>
+      </div>
+    );
+  }
+}
+
+//PropTypes
+// TodoItem.propTypes = {
+//   todo: propTypes.object.isRequired,
+// };
+
+const btnStyle = {
+  background: "#ff0000",
+  color: "#fff",
+  border: "none",
+  padding: "5px 9px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right",
+};
+
+export default TodoItem;
